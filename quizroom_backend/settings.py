c9a8d81aa1785784f08dab5f9fefb45fc7661334
Abdulrahman <abdoawwad043@gmail.com>
2025-08-03 00:36:01 +0300

    33333

--- quizroom_backend/settings.py
+++ quizroom_backend/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-83$7b80x+u*g0701mc5*)3^)u1qufwdzs&pi8twvx!$z=64-)8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
